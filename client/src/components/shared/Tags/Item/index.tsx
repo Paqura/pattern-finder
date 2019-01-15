3bfcc683a218feff721e68eb6f6f82b561dc3b63
paqura <bloodking666@mail.ru>
2019-01-15 20:33:50 +0400

    Add border style for tags

--- client/src/components/shared/Tags/Item/index.tsx
+++ client/src/components/shared/Tags/Item/index.tsx
@@ -7,6 +7,12 @@ export const
 	Item = styled.li`
 		background: #fafafa;
 		padding: 7px 12px;
+		border: 1px solid #fafafa;
+		transition: 120ms border cubic-bezier(0.075, 0.82, 0.165, 1);
+
+		&:hover {
+			border: 1px solid #dedede;
+		}
 	`;
 
 const
