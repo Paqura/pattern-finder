cbba0393ea110f850053055e5d674470e6c2c564
paqura <bloodking666@mail.ru>
2019-01-15 21:11:47 +0400

    Add server side template

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const keys = require('./settings/keys');
+
+mongoose.connect(keys.MONGO_URI, { useNewUrlParser: true,  })
+	.then(() => console.log('Database connected'))
+	.catch(err => console.log(err))
+
+mongoose.set('useCreateIndex', true);
+
+const authRoute = require('./routes/api/auth');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/api/auth', authRoute);
+
+app.use(compression());
+
+if(process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(
+			path.resolve(
+				__dirname, 'client', 'build', 'index.html'
+			)
+		)
+	})
+}
+
+module.exports = app;
---
+++ settings/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	MONGO_URI: 'mongodb://paqura616:paqura616@ds119663.mlab.com:19663/nanocell',
+	jwt: 'dev-jwt',
+};
